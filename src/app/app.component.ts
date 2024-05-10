1d9725e971436061037ec7af2345f25a526b6f64
manuel blanco <mandresblac@gmail.com>
2024-05-09 19:50:44 -0500

    Se agrega comentario linea 8  en archivo app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: true,// standalone significa que no usa modulos, todo se importa en la linea "imports" de abajo
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
